9251b890123e7677e81797865e4963d882d3c0ed
Bobby <118303001+Sillyquack@users.noreply.github.com>
2026-08-14 13:24:21 +0200

    feat: activate production candidate experience layer

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
+import ProductionCandidateOrchestrator from './experience/ProductionCandidateOrchestrator.jsx';
 import './styles.css';
 import './design-system/MeshExperienceSystem.css';
 import './components/EventOperatorExperience.css';
 import './components/EventOperatorEventPicker.css';
+import './experience/ProductionCandidateExperience.css';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <ProductionCandidateOrchestrator />
     <App />
   </React.StrictMode>,
 );
